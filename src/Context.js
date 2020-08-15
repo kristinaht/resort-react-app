71595a7fc8d63b8079a71855e950dc9a5eaa4b84
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-08-15 09:54:00 -0700

    create Context.js with RoomContext, RoomProvider, RoomConsumer & wrap with RoomProvider everything that is rendered in ReactDOM (index.js)

---
+++ src/Context.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+const RoomContext = React.createContext();
+//Provider (RoomContext) is responsible for allowing all components in component tree to access it. Component tree needs to be wrapped with the RoomContextProvider.
+
+//Consumer is used to access that information (info comming from RoomContext Provider)
+
+//RoomProvider is the Context.js
+class RoomProvider extends Component {
+  state= {};
+  render() {
+    return ( 
+      <RoomContext.Provider value='hello'>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+
+export { RoomProvider, RoomConsumer, RoomContext };
+//since ReactDOM is rendered in index.js, RoomProvider is going to be used in index.js
--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import { RoomProvider } from './Context'
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
   document.getElementById('root')
 );
 
