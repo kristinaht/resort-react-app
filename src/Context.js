064eef83fffb236ea12a5529d2dff337262934aa
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-08-18 14:43:49 -0700

    create Room & Loading components and import them into FeaturedRooms component , add state props in Context.js (RoomProvider) & also add componentDidMount, formatData methods

--- src/Context.js
+++ src/Context.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
+import items from './data'
 
 const RoomContext = React.createContext();
-//Provider (RoomContext) is responsible for allowing all components in component tree to access it. Component tree needs to be wrapped with the RoomContextProvider.
+//RoomContext is the context API.
+// Provider (RoomContext) is responsible for allowing all components in component tree to access it. Component tree needs to be wrapped with the RoomContextProvider.
 
 //Consumer is used to access that information (info comming from RoomContext Provider)
 
 //RoomProvider is the Context.js
 class RoomProvider extends Component {
   state= {
-    greeting: 'hello',
-    name: 'Kristina'
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true
   };
+
+  //getData
+
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    console.log(rooms);
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+      rooms, 
+      featuredRooms, 
+      sortedRooms: rooms, 
+      loading: false
+    })
+  }
+
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+
+      //let room object has all the properties it normally has, but we are overwritting the existing image if there is one & we are adding the id property as it is not normally part of the fields props (id comes from sys)
+      let room = {
+        ...item.fields, 
+        images,
+        id}
+
+      return room;
+    })
+    return tempItems;
+  }
   render() {
     return ( 
       <RoomContext.Provider value={{...this.state}}>
--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 import { RoomContext } from '../Context'
+import Loading from './Loading'
+import Room from './Room'
+import Title from './Title'
 
 export default class FeaturedRooms extends Component {
+  //static below can only be used in Class components
   static contextType = RoomContext;
   render() {
-    const { name, greeting } = this.context;
-  
+    let { loading, featuredRooms: rooms } = this.context;
+    
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />
+    });
+
     return (
-      <div>
-        {greeting} {name} from featured FeaturedRooms
-      </div>
+      <section className='reatured-rooms'>
+        <Title title='featured rooms' />
+        <div className='featured-rooms-center'>
+          { loading ? <Loading /> : rooms }
+        </div>
+      </section>
     )
   }
 }
