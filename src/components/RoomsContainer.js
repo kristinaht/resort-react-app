f8bcb925b684cce3cff540df060685ade0f20a18
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-08-24 11:36:53 -0700

    use RoomConsumer and pass a function that takes value; destructure value and render rooms, sortedRooms or loading

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -2,12 +2,30 @@ import React from 'react'
 import RoomFilter from './RoomFilter'
 import RoomList from './RoomList'
 
+import { RoomConsumer } from '../Context' //allows using the value
+import Loading from './Loading'
+
 export default function RoomsContainer() {
   return (
-    <div>
-      Hello from Rooms Container
-      <RoomFilter />
-      <RoomList />
-    </div>
+    <RoomConsumer>
+      { value => {
+          const { loading, sortedRooms, rooms } = value;
+
+          if(loading) {
+            return <Loading />
+          }
+
+          return (
+          <div>
+            Hello from Rooms Container
+            <RoomFilter rooms={rooms}/>
+            <RoomList rooms={sortedRooms}/>
+          </div>
+        );
+      }}
+    </RoomConsumer>
   )
 }
+
+
+//lines 11 - 25: this is a function that used to be used more before hooks, today hooks can be used instead. Still, it is fine to use the function as well
