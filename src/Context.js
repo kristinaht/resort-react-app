a17c167e57c349e6287781e7ad8aad2af9dddf78
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-08-24 19:46:08 -0700

    modify Context file: add to state props that are needed for filtering, modify componentDidMount to setState with correct price and size, create sceleton for the handleChange and filterRooms methods, return handleChange as part of the value in the Provider

--- src/Context.js
+++ src/Context.js
@@ -13,7 +13,16 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: true
+    loading: true,
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   };
 
   //getData
@@ -23,11 +32,17 @@ class RoomProvider extends Component {
     let rooms = this.formatData(items);
     console.log(rooms);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
+
     this.setState({
       rooms, 
       featuredRooms, 
       sortedRooms: rooms, 
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
     })
   }
 
@@ -53,11 +68,23 @@ class RoomProvider extends Component {
     return room;
   }
 
+  handleChange = event => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(type, name, value);
+  }
+
+  filterRooms = () => {
+    console.log('hello')
+  }
+
   render() {
     return ( 
       <RoomContext.Provider value={{
         ...this.state,
-        getRoom: this.getRoom
+        getRoom: this.getRoom,
+        handleChange: this.handleChange
         }}>
         {this.props.children}
       </RoomContext.Provider>
